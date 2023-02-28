e774c882b0894941647bdc9f0d2997db3cd76a53
dhruvarora561 <dhruvarora561@gmail.com>
2023-02-28 21:57:46 +0530

    fixed bugs

--- ivr/movies/urls.py
+++ ivr/movies/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path('answer', views.choose_theater, name='choose-theater'),
+   # path('answer', views.choose_theater, name='choose-theater'),
     path('choose-movie', views.choose_movie, name='choose-movie'),
     path('list-showtimes', views.list_showtimes, name='list-showtimes'),
 ]
